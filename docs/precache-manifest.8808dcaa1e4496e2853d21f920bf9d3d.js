baf004f5f053b74d6d16659fe9ee8342aa51244d
funawo <funawo@example.com>
2020-06-16 09:48:43 +0900

    Release

---
+++ docs/precache-manifest.8808dcaa1e4496e2853d21f920bf9d3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed9d4f33d8851e179b7782aee9ce076",
+    "url": "/html-practice/index.html"
+  },
+  {
+    "revision": "4b8df738b1db2bce2847",
+    "url": "/html-practice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad3103228493264ea4f5",
+    "url": "/html-practice/static/js/2.5d08d882.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/html-practice/static/js/2.5d08d882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8df738b1db2bce2847",
+    "url": "/html-practice/static/js/main.6cdd8a51.chunk.js"
+  },
+  {
+    "revision": "5b62b0877a74938a12dc",
+    "url": "/html-practice/static/js/runtime-main.a54f59c2.js"
+  }
+]);
